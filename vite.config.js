6cb1d9ca1e218d53f546ccc4f5fd33ff908e153b
leo-dev19 <leonardogchavez.2006@gmail.com>
2026-05-03 00:30:55 -0500

    Feature/cambio de dependencias para despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portafolio/', // cambia esto si tu repo se llama distinto
+  base: '/portafolio/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 });
